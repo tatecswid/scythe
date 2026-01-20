31d80bfbbe34142fb2f93565b8a71152e8b8c5bc
tatecswid <tatecswid@gmail.com>
2026-01-19 21:28:52 -0500

    polish up untextured raycasting

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 
+
 // FPS
 let FPS = 30;
 let interval = 1000 / FPS;
@@ -12,28 +13,31 @@ const MAP_HEIGHT = 16;
 const SCREEN_WIDTH = 300;
 const SCREEN_HEIGHT = 200;
 const WORLD_MAP = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1],
     [1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 3, 2, 0, 0, 0, 0, 0, 1],
+    [3, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+canvas.width = SCREEN_WIDTH;
+canvas.height = SCREEN_HEIGHT;
 
 // player properties
 let posX = 9.7, posY = 5;
 let dirX = -1, dirY = 0;
-let planeX = 0, planeY = 0.50;
+let planeX = 0, planeY = 0.66;
 let moveSpeed = 0.1;
 let rotSpeed = 0.03;
 
@@ -44,9 +48,6 @@ const controls = {
     d : false,
 }
 
-// camera properties
-const CAMERA_HEIGHT = 0.8;
-const CAMERA_OFFSET = 20;
 
 // player controls
 window.addEventListener("keydown", (e) => { if(e.key in controls) { controls[e.key] = true } });
@@ -68,11 +69,8 @@ var img = new Image(64,64);
 img.src = "background.png"
 
 function drawScreen() {
-    ctx.drawImage(img, 0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
-    /*
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "grey";
     ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-    */
 }
 
 function updatePlayer() {
@@ -105,8 +103,8 @@ function updatePlayer() {
 }
 
 function raycast() {
-    for(let x = 0; x < SCREEN_WIDTH; x++) {
-        let cameraX = 2 * x / SCREEN_WIDTH - 1;
+    for(let ray = 0; ray < SCREEN_WIDTH; ray++) {
+        let cameraX = 2 * ray / SCREEN_WIDTH - 1;
         let rayDirX = dirX + planeX * cameraX;
         let rayDirY = dirY + planeY * cameraX;
 
@@ -156,14 +154,14 @@ function raycast() {
             if(WORLD_MAP[mapX][mapY] > 0) hit = 1;
         }
 
-        perpWallDist = (side == 0)? (sideDistX - deltaDistX) : (sideDistY - deltaDistY);
+        perpWallDist = (side == 0) ? (sideDistX - deltaDistX) : (sideDistY - deltaDistY);
 
         let lineHeight = Math.floor(SCREEN_HEIGHT / perpWallDist)
 
-        let drawStart = -lineHeight * CAMERA_HEIGHT + SCREEN_HEIGHT / 2 - CAMERA_OFFSET;
+        let drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
         if(drawStart < 0) drawStart = 0;
-        let drawEnd = lineHeight * CAMERA_HEIGHT + SCREEN_HEIGHT / 2 - CAMERA_OFFSET;
-        if(drawEnd >= SCREEN_HEIGHT) drawEnd = SCREEN_HEIGHT - 1;
+        let drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
+        if(drawEnd >= SCREEN_HEIGHT) drawEnd = SCREEN_HEIGHT;
 
         let color = 'white';
 
@@ -175,12 +173,12 @@ function raycast() {
                 color = 'green';
                 break;
             case 3:
-                color = 'grey';
+                color = 'orange';
                 break;
         }
 
         if(side == 1) color = 'dark' + color
         ctx.fillStyle = color;
-        ctx.fillRect(x, drawStart, 1, drawEnd-drawStart)
+        ctx.fillRect(ray, drawStart, 1, drawEnd-drawStart)
     }
 }
